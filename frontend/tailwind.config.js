573793145563e756350dc3a5b541d4fe7caf4c99
Uma-MCMXC <petkong28@gmail.com>
2025-01-17 11:35:11 +0700

    frontend: build flowbite

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // เพิ่มโฟลเดอร์ app
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}", // เพิ่มโฟลเดอร์ app
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("flowbite/plugin")],
 };
