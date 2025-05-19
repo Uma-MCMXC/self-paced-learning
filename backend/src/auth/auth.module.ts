adf378d9ced975a086208cce19efca3da0b1be04
Uma-MCMXC <petkong28@gmail.com>
2025-05-19 10:52:41 +0700

    backend: ConfigModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,13 +5,21 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'your-secret-key', // ควรเก็บใน .env
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '1d',
+        },
+      }),
     }),
     UsersModule,
   ],
